2054c7644bd34d430c4c29753cda12691498770e
okoking <144639739+okoking@users.noreply.github.com>
2025-09-01 10:28:02 +0900

    first take

---
+++ Bowling/Assets/Script/player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 m_RotationSpeed = new Vector3(0, 0, 0); // 1•bŠÔ‚Ì‰ñ“]Šp“x
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(m_RotationSpeed * Time.deltaTime);
+    }
+}
