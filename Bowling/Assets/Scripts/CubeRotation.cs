a1c32d77f32fad4b90862c58e34e90c9430e8539
okoking <144639739+okoking@users.noreply.github.com>
2025-09-01 13:02:31 +0900

    pad

--- Bowling/Assets/Scripts/CubeRotation.cs
+++ Bowling/Assets/Scripts/CubeRotation.cs
@@ -4,6 +4,8 @@ public class CubeRotation : MonoBehaviour
 {
     public Vector3 m_RotationSpeed = new Vector3(0, 0, 0); // 1•bŠÔ‚Ì‰ñ“]Šp“x
 
+    const float ROTA_SCALE = 1000.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,13 @@ public class CubeRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(m_RotationSpeed * Time.deltaTime);
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        if ((hori != 0) || (vert != 0))
+        {
+            m_RotationSpeed.x = vert * ROTA_SCALE;
+            m_RotationSpeed.y = -hori * ROTA_SCALE;
+            transform.Rotate(m_RotationSpeed * Time.deltaTime);
+        }
     }
 }
