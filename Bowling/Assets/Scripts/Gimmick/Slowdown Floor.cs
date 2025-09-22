65f4521c3c6b7782626dadbc2c9d628dab925806
tubasajp <144640052+tubasajp@users.noreply.github.com>
2025-09-22 12:43:04 +0900

    ギミック（スローの床）途中

--- Bowling/Assets/Scripts/Gimmick/Slowdown Floor.cs
+++ Bowling/Assets/Scripts/Gimmick/Slowdown Floor.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SlowdownFloor : MonoBehaviour
 {
+    public float SlowSpeed = 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +13,21 @@ public class SlowdownFloor : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Debug.Log("すり抜けスローダウン" + other.gameObject.name);
+            // 現在の速度を取得
+            Rigidbody rb = other.attachedRigidbody;
+
+            //オブジェクトの直線方向の移動速度
+            Vector3 originalVelocity = rb.linearVelocity;
+            // 減速倍率をかけて新しい速度を計算
+            Vector3 slowedVelocity = originalVelocity * SlowSpeed;
+            //減速を適用
+            rb.linearVelocity = slowedVelocity;
+        }
     }
 }
