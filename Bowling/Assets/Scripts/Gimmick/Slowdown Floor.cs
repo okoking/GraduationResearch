f52865b64ed35646259c25812e325c904ebb5b0c
tubasajp <144640052+tubasajp@users.noreply.github.com>
2025-10-01 09:49:32 +0900

    ギミック（スローダウン）完成

--- Bowling/Assets/Scripts/Gimmick/Slowdown Floor.cs
+++ Bowling/Assets/Scripts/Gimmick/Slowdown Floor.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SlowdownFloor : MonoBehaviour
 {
-    public float SlowSpeed = 0.2f;
+    public float SlowSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
