1f88c826c9756dfb09c0e0274607d6733cd2d2ec
tubasajp <144640052+tubasajp@users.noreply.github.com>
2025-10-02 15:20:39 +0900

    ギミックマネージャー未完成

--- Bowling/Assets/Scripts/Gimmick/GimmickManager.cs
+++ Bowling/Assets/Scripts/Gimmick/GimmickManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GimmickManager : MonoBehaviour
 {
+    [SerializeField] private GameObject WallPrefab;
+    [SerializeField] private Transform spawnPoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,10 +14,23 @@ public class GimmickManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WallAdd();
     }
     void WallAdd()
     {
-        
+        bool WallIsUse = false;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WallIsUse = true;
+
+        }
+        if (WallPrefab != null && spawnPoint != null)
+        {
+            Instantiate(WallPrefab, spawnPoint.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("WallPrefab Ç‹ÇΩÇÕ spawnPoint Ç™ê›íËÇ≥ÇÍÇƒÇ¢Ç‹ÇπÇÒÅB");
+        }
     }
 }
