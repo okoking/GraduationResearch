664787426230f73278d9d0f48f17d59abb8ba88d
okoking <144639739+okoking@users.noreply.github.com>
2025-10-28 11:18:36 +0900

    Beam

--- Bowling/Assets/Scripts/Player/KariBeam.cs
+++ Bowling/Assets/Scripts/Player/KariBeam.cs
@@ -1,17 +1,73 @@
 using UnityEngine;
+using System.Collections;
 
 public class KariBeam : MonoBehaviour
 {
-    [SerializeField] private float speed = 20f;
-    [SerializeField] private float lifetime = 3f;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private float beamLength = 50f;
+    [SerializeField] private float beamWidth = 0.2f;
+    [SerializeField] private float beamDuration = 3f; // 何秒間出し続けるか
+    [SerializeField] private LayerMask hitMask;
+
+    private bool isFiring = false;
+    private Camera mainCam;
+    private BeamCamera beamCamera;
 
     void Start()
     {
-        Destroy(gameObject, lifetime);
+        lineRenderer.enabled = false;
+        lineRenderer.startWidth = beamWidth;
+        lineRenderer.endWidth = beamWidth;
+        mainCam = Camera.main;
+        beamCamera = GetComponent<BeamCamera>();
     }
 
     void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        // ボタン押したらビーム発射
+        if (Input.GetKeyDown("joystick button 5") && !isFiring)
+        {
+            StartCoroutine(FireBeam());
+        }
+    }
+
+    IEnumerator FireBeam()
+    {
+        if (beamCamera.isSootBeam)
+        {
+
+            isFiring = true;
+            lineRenderer.enabled = true;
+
+            float timer = 0f;
+            while (timer < beamDuration)
+            {
+                Vector3 start = transform.position;
+                start.y += 1;
+                Ray ray = mainCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); // 中央(0.5,0.5)
+                Vector3 end = ray.origin + ray.direction * beamLength;
+
+                //// Raycastで命中判定
+                //if (Physics.Raycast(start, transform.forward, out RaycastHit hit, beamLength, hitMask))
+                //{
+                //    end = hit.point;
+                //    // 当たった敵に処理
+                //    if (hit.collider.CompareTag("Enemy"))
+                //    {
+                //        // EnemyスクリプトのTakeDamageを呼ぶなど
+                //        // hit.collider.GetComponent<Enemy>()?.TakeDamage(10);
+                //    }
+                //}
+
+                lineRenderer.SetPosition(0, start);
+                lineRenderer.SetPosition(1, end);
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            lineRenderer.enabled = false;
+            isFiring = false;
+        }
     }
 }
