8068b1d5e580ae71e8258ee3ba71031b7be6fefd
shinf <144640093+namaeanyting@users.noreply.github.com>
2025-12-15 11:53:18 +0900

    Title Button sukosi

--- Bowling/Assets/Scripts/Title/Title.cs
+++ Bowling/Assets/Scripts/Title/Title.cs
@@ -1,33 +1,36 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
     //Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Debug.Log("ゲーム開始");
+        //UIシーンを合わせている
+        SceneManager.LoadScene("TitleUi", LoadSceneMode.Additive);
     }
 
     //Update is called once per frame
     void Update()
     {
-        if(InputManager.Instance.Jump)
-        {
-            Debug.Log("ジャンプが押されたよ");
-        }
 
-        //オプションシーンへ
-        if (InputManager.Instance.Option)
-        {
-            SceneController.Instance.ChangeState(GameState.Option);
-        }
-        
-        //ステージセレクトシーンへ
-        if (InputManager.Instance.Enter)
-        {
-            SceneController.Instance.ChangeState(GameState.StageSelect);
-        }
+        //if (InputManager.Instance.Jump)
+        //{
+        //    Debug.Log("ボタン押された");
+        //}
 
-       
+        ////オプションシーンへ
+        //if (InputManager.Instance.Option)
+        //{
+        //    SceneController.Instance.ChangeState(GameState.Option);
+        //}
+
+        ////ステージセレクトシーンへ
+        //if (InputManager.Instance.Enter)
+        //{
+        //    SceneController.Instance.ChangeState(GameState.StageSelect);
+        //}
+
     }
 }
---
+++ Bowling/Assets/Scripts/Title/TitleUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // ボタンが押された場合、今回呼び出される関数
+
+    public void OnClick()
+    {
+        Debug.Log("押された!");
+    }
+}
