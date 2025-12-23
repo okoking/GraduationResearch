cf94a9ebe774f0eda692b2bf61ca113c84dac388
shinf <144640093+namaeanyting@users.noreply.github.com>
2025-12-23 17:11:11 +0900

    タイトルとリザルト　いったん保存

--- Bowling/Assets/Scripts/Result/ClearUI.cs
+++ Bowling/Assets/Scripts/Result/ClearUI.cs
@@ -1,23 +1,42 @@
+using System;
 using UnityEngine;
 
 public class ClearUI : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
+    public float fadeTime = 0.001f;
+    float t = 0f;
+    bool startFlag; //透過を始めるか
 
     void Start()
     {
         canvasGroup.alpha = 0f;
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
+
+        startFlag = false;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //仮のトリガー
+        //本来はゲームがクリアしたら
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            canvasGroup.alpha = 1f;
+            startFlag = true;
         }
+
+        Fead();
     }
 
+    void Fead()
+    {
+        if (startFlag && t < 1)
+        {
+            t += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(0f, 1f, t / fadeTime);
+        }
+    }
 }
+
 
