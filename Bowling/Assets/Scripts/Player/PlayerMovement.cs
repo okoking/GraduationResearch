c63abb247e290097799e2b05d219bc6f171d3aef
penta0410 <144640030+penta0410@users.noreply.github.com>
2026-01-13 02:20:01 +0900

    a

--- Bowling/Assets/Scripts/Player/PlayerMovement.cs
+++ Bowling/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private bool wasGroundEx;
 
     private Vector3 wallNormal;
-
+  
     void Start()
     {
         beamInfo = GetComponent<SpecialBeam>();
