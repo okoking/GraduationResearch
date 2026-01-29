8df344abda5440327163354e811b9163b929e0dc
OkayamaMayu <2309406@s.asojuku.ac.jp>
2026-01-29 14:44:38 +0900

    20260129_add
    SE登録

--- Bowling/Assets/Scripts/Managers/Sound/SoundList.cs
+++ Bowling/Assets/Scripts/Managers/Sound/SoundList.cs
@@ -7,6 +7,9 @@ public class SoundData
     [Tooltip("サウンドID")] public string soundID;
     [Tooltip("オーディオクリップ")] public AudioClip audioClip;
     [Tooltip("音量")][Range(0.0f, 1.0f)] public float soundVolume = 1f;
+    [Tooltip("備考欄")]
+    [TextArea(0, 4)]
+    public string comment;
 }
 
 [System.Serializable]
@@ -14,6 +17,9 @@ public class AudioSourceData
 {
     [Tooltip("AudioSourceID")] public string audioSourceID;
     [Tooltip("AudioSource")] public AudioSource audioSource;
+    [Tooltip("備考欄")]
+    [TextArea(0, 4)]
+    public string comment;
 }
 
 [CreateAssetMenu(menuName = "System/SoundList")]
