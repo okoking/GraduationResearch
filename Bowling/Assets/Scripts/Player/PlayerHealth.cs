c08b6c97813ba6b60f52351510e525fdd76ec235
okoking <144639739+okoking@users.noreply.github.com>
2026-02-12 11:31:21 +0900

    Update PlayerHealth.cs

--- Bowling/Assets/Scripts/Player/PlayerHealth.cs
+++ Bowling/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -9,15 +10,42 @@ public class PlayerHealth : MonoBehaviour
     [Header("デバッグ用")]
     [SerializeField] private int Damage = 10;
 
+    private bool isInvincible = false;
+    public float invincibleTime = 1f; // 無敵時間（秒）
+    Renderer[] renderers;
 
     void Start()
     {
         currentHealth = maxHealth;  // ゲーム開始時に全回復
+        renderers = GetComponentsInChildren<Renderer>();
+    }
+
+    private IEnumerator InvincibleCoroutine()
+    {
+        isInvincible = true;
+
+        float timer = 0f;
+        Renderer renderer = GetComponentInChildren<Renderer>();
+
+        while (timer < invincibleTime)
+        {
+            foreach (Renderer r in renderers)
+            {
+                r.enabled = !r.enabled;
+            }
+            yield return new WaitForSeconds(0.1f);
+            timer += 0.1f;
+        }
+
+        renderer.enabled = true;
+        isInvincible = false;
     }
 
     // ダメージを受ける
     public void TakeDamage(int damage)
     {
+        if (isInvincible) return; // 無敵中ならダメージ受けない
+
         currentHealth -= damage;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // 0～maxに制限
 
@@ -25,6 +53,8 @@ public class PlayerHealth : MonoBehaviour
         {
             Die();
         }
+
+        StartCoroutine(InvincibleCoroutine());
     }
 
     [ContextMenu("ダメージ与える")]
